fcc39cb4569b3a23dcb1135f0d5546e3b99f2a61
Apisart Chanpuang <c.apisart@gmail.com>
2021-07-04 17:19:25 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showIcon(IconNames.Heart)
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+basic.pause(1000)
+music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Forever)
+for (let index = 0; index < 1; index++) {
+    basic.showString("Hello!")
+}
+basic.showIcon(IconNames.Happy)
+music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
